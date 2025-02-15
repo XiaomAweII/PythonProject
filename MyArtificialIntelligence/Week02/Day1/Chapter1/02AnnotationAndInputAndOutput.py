435d96464d60c97d5342a38265eb0c351ae3fb0b
XiaomAweII <805122541@qq.com>
2025-02-15 22:17:30 +0800

    Python基础与程序流程控制-第1章 基础语法与输入输出

---
+++ MyArtificialIntelligence/Week02/Day1/Chapter1/02AnnotationAndInputAndOutput.py
@@ -0,0 +1,24 @@
+a = 1
+# b=2
+c = 3
+
+'''
+d=4
+e=5
+'''
+f = 6
+"""
+g=7
+h=8
+"""
+i = 9
+
+print("AI")
+print("Hello", "World")
+print("Hello", "World", sep="-")
+print("Hello", end="!")
+
+result = input()
+print("result", result)
+result = input("请输入：")
+print("result", result)
---
+++ MyArtificialIntelligence/Week02/Day1/Chapter1/03Practice.py
@@ -0,0 +1,2 @@
+name = input("你是谁？")
+print("欢迎学习Python", name, sep=",", end="!")
